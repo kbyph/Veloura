54aa4ae494eac79b8e042b0bab7c861d79f84d49
Kobe Pho <kobeypho@gmail.com>
2023-11-28 17:59:04 -0800

    Reformatted magazine publishing date

--- components/Recommendation.jsx
+++ components/Recommendation.jsx
@@ -17,11 +17,12 @@ const recommendationData = [
     image: '/recommendation/nobu.png',
     location: 'Cabo San Lucas, Mexico',
     desc: 'Experience the epitome of luxury at Nobu Hotel Los Cabos, where sleek design meets Pacific tranquility. Indulge in world-class dining, breathtaking ocean views, and a harmonious blend of sophistication and relaxation on the pristine shores of the Baja Peninsula.',
-    price: 400 ,
+    price: 400,
     testimonial: {
       message: '"Nobu Hotel Los Cabos exceeded all expectations. The fusion of sophisticated design, top-notch amenities, and the exceptional Nobu dining experience made our stay an unforgettable blend of luxury and indulgence."',
       personName: 'Emily Rodriguez',
-      title: 'Travel Feature Contributor, "Departures Magazine." May. 2023',
+      title: 'Travel Feature Contributor, "Departures Magazine."',
+      date: 'May. 2023',
     },
   },
   {
@@ -29,11 +30,12 @@ const recommendationData = [
     image: '/recommendation/maui.jpg',
     location: 'Maui, Hawaii, USA',
     desc: 'Four Seasons Resort Maui at Wailea is a tropical paradise with stunning oceanfront accommodations. Enjoy the beauty of Hawaii with luxurious amenities and world-class service.',
-    price: 950 ,
+    price: 950,
     testimonial: {
       message: '"A haven of tranquility! Four Seasons Maui at Wailea offers a blend of luxury and Hawaiian serenity. The lush gardens, attentive staff, and the mesmerizing sunset views made our stay an extraordinary escape."',
       personName: 'David Patel',
-      title: 'Destination Specialist, "Luxury Travel Advisor." Dec. 2019',
+      title: 'Destination Specialist, "Luxury Travel Advisor."',
+      date: 'Dec. 2019',
     },
   },
   {
@@ -45,7 +47,8 @@ const recommendationData = [
     testimonial: {
       message: '"Amanemu is a Japanese masterpiece! From the serene hot springs to the minimalist luxury of the accommodations, every element exuded tranquility and refinement. The integration of traditional Japanese aesthetics, impeccable service, and the calming onsen waters made our stay a truly authentic and luxurious escape."',
       personName: 'Michael Anderson',
-      title: 'Travel Writer, "Condé Nast Traveler." Mar. 2022',
+      title: 'Travel Writer, "Condé Nast Traveler."',
+      date: 'Mar. 2022',
     },
   },
 ];
@@ -113,6 +116,7 @@ const Recommendation = () => {
                         {hotel.testimonial.personName}
                       </p>
                       <p>{hotel.testimonial.title}</p>
+                      <p>{hotel.testimonial.date}</p>
                     </div>
                   </div>
                 </div>
